b1bcfc408eb773690c946198939cfa08d3731f09
NanneB321 <48436740+NanneB321@users.noreply.github.com>
2019-04-18 14:01:42 +0200

    nog niet af

---
+++ src/quizopdracht/Opdracht231A.java
@@ -0,0 +1,17 @@
+package quizopdracht;
+import java.util.Scanner;
+public class Opdracht231A {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("Type een zin: ");
+		Scanner scanner = new Scanner(System.in);
+		String username = scanner.nextLine().toUpperCase();
+		new StringBuilder("username").reverse().toString();
+		System.out.println("Resultaat " + username);
+		
+		
+	}
+
+}
