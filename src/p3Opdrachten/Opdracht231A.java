51d6e0170cbcca644781727402478d17ff0916e5
NanneB321 <161105@student.aventus.nl>
2019-05-12 15:40:22 +0200

    Opdrachten verplaats

---
+++ src/p3Opdrachten/Opdracht231A.java
@@ -0,0 +1,29 @@
+package p3Opdrachten;
+
+import java.util.Scanner;
+import java.lang.*; 
+import java.io.*; 
+import java.util.*; 
+public class Opdracht231A {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("Type een zin: ");
+		Scanner scanner = new Scanner(System.in);
+		String username = scanner.nextLine().toUpperCase();
+		new StringBuilder("username").reverse().toString();
+		
+        String input = username; 
+        
+        StringBuilder username1 = new StringBuilder(); 
+  
+        username1.append(input); 
+  
+        username1 = username1.reverse(); 
+  
+        System.out.println("Resultaat " + username1);
+		
+	}
+
+}
