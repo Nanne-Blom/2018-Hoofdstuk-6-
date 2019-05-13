f0f041dfbfe2cd4130b23b2e970a82c29f10fd52
NanneB321 <161105@student.aventus.nl>
2019-05-13 20:30:38 +0200

    Bezig met sorteeralgoritme

---
+++ src/p3Opdrachten/S232H8OpdrachtA.java
@@ -0,0 +1,23 @@
+package p3Opdrachten;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections; 
+import java.lang.Math; 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.*;
+import p3Opdrachten.QuickSort;
+public class S232H8OpdrachtA {
+ static Integer AAA;
+		static Integer BBB;
+	public static void main(String [] args) {
+	
+		List<Integer> numbers = new ArrayList<>();
+		       
+
+				 for (int i = 0; 1 < 10; i++) {
+			numbers.add(i = ThreadLocalRandom.current().nextInt(0, 10000 + 1));
+
+}
+}
+}
