51936676695815501c82499819fd3ce36e96e48c
Jan-Willem Roode <jgp.roode@upcmail.nl>
2019-05-23 13:27:28 +0200

    bubblesort toegevoegd....
    nu aanpassen aan jouw situatie..
    
    succes

--- src/p3Opdrachten/S232H8OpdrachtA.java
+++ src/p3Opdrachten/S232H8OpdrachtA.java
@@ -1,41 +1,72 @@
 package p3Opdrachten;
 import java.util.ArrayList;
-import p3Opdrachten.BubbleSort;
-import java.util.List;
-import java.util.Collections; 
 import java.lang.Math; 
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.*;
-import p3Opdrachten.QuickSort;
+
+
 public class S232H8OpdrachtA {
+	final static int maxAantal=100000;
+	final static int laagsteWaarde=1;
+	final static int hoogsteWaarde=10000;
+	static ArrayList<Integer> nummerLijst = new ArrayList<>();
+
+
 	public static void main(String [] args) {
-	 
-			usingRandomClass();
+
+		nummerLijst =usingRandomClass(maxAantal, laagsteWaarde, hoogsteWaarde);
+	}
+
+
+
+
+
+
+	public static ArrayList<Integer> usingRandomClass(int maxAantal, int laagstewaarde, int hoogstewaarde) {
+		ArrayList<Integer> lijst = new ArrayList<>();
+		
+		Random randomGenerator = new Random();
+
+
+		for (int i =0;i<=maxAantal;i++) {
+			
+			lijst.add(    (int)( Math.random()*(hoogsteWaarde-laagsteWaarde))+laagsteWaarde    );
+	
+			System.out.println(""+ (int) lijst.get(i)); 		
 		}
-	static int a;
-	static ArrayList<Integer> Nummers = new ArrayList<>();
-	
-	
-	
-	static void usingRandomClass() {
-			Random randomGenerator = new Random();
 
-			
-			
-
-	 int a = 0;
-			while (a < 100000) {
-				int randomInt = randomGenerator.nextInt(10000) + 1;
-	Nummers.add(randomInt);
-	a ++;
-
-			}		
-			System.out.print(Nummers);
-
-
-		    
-			
+		
+		return lijst;
 	}
-	}
+	
+	
+	 // EEN ARRALIST OMZETTEN NAAR EEN ARRAY
+	  // LASTIG IS DAT HET ARRAY DUS EEN VAST AANTAL ELEMENTEN HEEFT EN EEN ARRAYLIST IN PRINCIOE NIET
+	  // DAAROM WORDT DE SIZE VAN HET ARRAYLIST BEREKEND EN GEBRUIKT OM DE GROOTE VAN HET ARRAY TE BEPALEN
+	  public static int[] convertertoArray(ArrayList list){
+		  int lengte =list.size();
+		  int tabel[] = new int[lengte];
+		  
+		  for (int i=0;i<lengte;i++) {
+			//LET OP TYPECAST IS NOODZAKELIJK ZIE PRESENTATIE 
+			tabel[i]=(int)list.get(i);  
+		  }	 
+		  //EEN ARRAY TERUG GEVEN
+		  return tabel;
+	  }
+	
+	
+/*
+		for( int bovengrens = tabelBubbleSorted.size()- 1; bovengrens >= 1; bovengrens-- ) {
+			for( int index = 0; index < bovengrens; index++ ) {
+				if( (int) tabelBubbleSorted.get(index) > (int)tabelBubbleSorted.get(index+1) ) {
+  				int hulp = (int) tabelBubbleSorted.get(index);
+  				tabelBubbleSorted.set(index,tabelBubbleSorted.get(index+1) );
+  				tabelBubbleSorted.set(index+1, hulp);
+				}
+			}
+		}
+	
+	*/
+	
 
+}
