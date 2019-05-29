7235e53c2b86f0baa0c99bda8e7a5f5233a2db77
Jan-Willem Roode <jgp.roode@upcmail.nl>
2019-05-29 10:59:28 +0200

    nieuwe opdracht

--- src/p3Opdrachten/S232H8OpdrachtA.java
+++ src/p3Opdrachten/S232H8OpdrachtA.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class S232H8OpdrachtA {
-	final static int maxAantal=0;
+	//	final static int maxAantal=0;
 	final static int laagsteWaarde=0;
 	final static int hoogsteWaarde=10000;
 	static ArrayList<Integer> nummerLijst = new ArrayList<>();
@@ -13,24 +13,30 @@ public class S232H8OpdrachtA {
 
 	public static void main(String [] args) {
 
+
+		int maxAantal= leesGeheelGetal();
 		nummerLijst =usingRandomClass(maxAantal, laagsteWaarde, hoogsteWaarde);
 	}
 
+	public static int leesGeheelGetal () {
 
+		Scanner inputScanner = new Scanner(System.in);
+		System.out.println("Voer een getal boven de 0 in! (bij hoge getallen kan het meerdere minuten duren).");
+
+		String input = inputScanner.nextLine();
+
+		inputScanner.close();
+		//try en catch !!
+		int inputInt = Integer.parseInt(input);	
+		return inputInt;
+
+	}
 
 
 	public static ArrayList<Integer> usingRandomClass(int maxAantal, int laagstewaarde, int hoogstewaarde) {
 		ArrayList<Integer> lijst = new ArrayList<>();
-			 
-		Scanner inputScanner = new Scanner(System.in);
-	 System.out.println("Voer een getal boven de 0 in! (bij hoge getallen kan het meerdere minuten duren).");
-	 
-	    String input = inputScanner.nextLine();
-	    
-	    inputScanner.close();
-	    int inputInt = Integer.parseInt(input);	
-	    System.out.println("");
-maxAantal = 0 + inputInt;
+
+	//	maxAantal = 0 + inputInt;
 		for (int i = 0;i <= maxAantal;) {
 			i++;
 			lijst.add(    (int)( Math.random()*(hoogsteWaarde-laagsteWaarde))+laagsteWaarde    );
@@ -41,36 +47,36 @@ maxAantal = 0 + inputInt;
 		for( int hoogsteWaarde = lijst.size()- 1; hoogsteWaarde >= 1; hoogsteWaarde-- ) {
 			for( int index = 0; index < hoogsteWaarde; index++ ) {
 				if( (int) lijst.get(index) > (int)lijst.get(index+1) ) {
-  				int hulp = (int) lijst.get(index);
-  				lijst.set(index,lijst.get(index+1) );
-  				lijst.set(index+1, hulp);
-  				
-  				
+					int hulp = (int) lijst.get(index);
+					lijst.set(index,lijst.get(index+1) );
+					lijst.set(index+1, hulp);
+
+
 				}
 			}
 		}
-		
+
 		System.out.println(""+ lijst ); 
 		System.out.print(maxAantal + " Nummers gesorteerd!");
-return lijst;
+		return lijst;
 
 	}
-	
-	
-	 // EEN ARRALIST OMZETTEN NAAR EEN ARRAY
-	  // LASTIG IS DAT HET ARRAY DUS EEN VAST AANTAL ELEMENTEN HEEFT EN EEN ARRAYLIST IN PRINCIOE NIET
-	  // DAAROM WORDT DE SIZE VAN HET ARRAYLIST BEREKEND EN GEBRUIKT OM DE GROOTE VAN HET ARRAY TE BEPALEN
-	  //public static int[] convertertoArray(ArrayList list){
-		//  int lengte =list.size();
-		 // int tabel[] = new int[lengte];
-		  
-		 // for (int i=0;i<lengte;i++) {
-			//LET OP TYPECAST IS NOODZAKELIJK ZIE PRESENTATIE 
-			//tabel[i]=(int)list.get(i);  
-		//  }	 
-		  //EEN ARRAY TERUG GEVEN
-		//  return tabel;
 
-	
+
+	// EEN ARRALIST OMZETTEN NAAR EEN ARRAY
+	// LASTIG IS DAT HET ARRAY DUS EEN VAST AANTAL ELEMENTEN HEEFT EN EEN ARRAYLIST IN PRINCIOE NIET
+	// DAAROM WORDT DE SIZE VAN HET ARRAYLIST BEREKEND EN GEBRUIKT OM DE GROOTE VAN HET ARRAY TE BEPALEN
+	//public static int[] convertertoArray(ArrayList list){
+	//  int lengte =list.size();
+	// int tabel[] = new int[lengte];
+
+	// for (int i=0;i<lengte;i++) {
+	//LET OP TYPECAST IS NOODZAKELIJK ZIE PRESENTATIE 
+	//tabel[i]=(int)list.get(i);  
+	//  }	 
+	//EEN ARRAY TERUG GEVEN
+	//  return tabel;
+
+
 
 }
